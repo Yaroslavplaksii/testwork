ef7c579768cb1cad50542334ee18571accfed772
Yaroslav <yaroslav.plaksii@gmail.com>
2017-11-08 20:51:27 +0200

    classes

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 class Developer{
     const MAX_TASKS = 10;
+    
+    public static $user_name;
+    
     public function add_task(){
+        
+    }
+    public function tasks(){
+        
+    }
+    public function status(){
         
     }
     public function work(){
@@ -10,9 +19,20 @@ class Developer{
     public function can_add_task(){
         
     }
-    public function __construct($name){
+    public function can_work(){
         
     }
+    public function __construct($name){
+        self::$user_name = $name;
+    }
+}
+class JuniorDeveloper extends Developer{
+    const MAX_TASKS = 5;
+    
+}
+class SeniorDeveloper extends Developer{
+    const MAX_TASKS = 15;
+    
 }
 
 ?>
