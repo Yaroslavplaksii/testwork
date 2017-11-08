aa3ba63b5c5ea49aa7f6e23801fae5b33ea72fb9
Yaroslav <yaroslav.plaksii@gmail.com>
2017-11-08 22:02:20 +0200

    start

--- index.php
+++ index.php
@@ -3,24 +3,48 @@ class Developer{
     const MAX_TASKS = 10;
     
     public static $user_name;
+    public static $user_status = 0;
+    public static $user_tasks = 0;
     
-    public function add_task(){
-        
+    public function add_task($name_task){
+      if($this->can_add_task()){
+          self::$user_tasks++;            
+      }  
     }
     public function tasks(){
         
     }
     public function status(){
-        
+        if(self::$user_status == 0){
+            return 'Вільний';
+        }elseif(self::$user_status == 1){
+            return 'Працюю';
+        }else{
+            return 'Зайнятий';
+        }
     }
-    public function work(){
-        
+    public function work(){        
+        if(self::$user_tasks>0){
+            self::$user_tasks--;
+            self::$user_status = 1;
+            //return  self::$user_name." ".$name_task   ;
+        }else{
+            return 'Роботи немає';
+        }        
     }
     public function can_add_task(){
-        
+        if(self::$user_tasks<self::MAX_TASKS){
+            return true;
+        }else{
+            return false;
+        }
     }
     public function can_work(){
-        
+        if(self::$user_status == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
     public function __construct($name){
         self::$user_name = $name;
