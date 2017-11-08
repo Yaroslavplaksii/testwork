5a4b9f25ae55a83ceeada8b5273b95f15ec0c015
Yaroslav <yaroslav.plaksii@gmail.com>
2017-11-08 20:26:11 +0200

    methods

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-
+class Developer{
+    const MAX_TASKS = 10;
+    public function add_task(){
+        
+    }
+    public function work(){
+        
+    }
+    public function can_add_task(){
+        
+    }
+    public function __construct($name){
+        
+    }
+}
 
 ?>
